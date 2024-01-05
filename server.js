e3db402a343a0b854eb73d5531366d27bf53f584
madhumech1405 <madhumech1405@gmail.com>
2024-01-06 00:24:14 +0530

    simple web server server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const http =require('http')
+const hostname='127.0.0.1';
+const port =3000;
+const server=http.createServer((req,res)=>{
+    res.statusCode=200;
+    res.setHeader("content",'text/plain');
+    res.end('welcome creater\n');
+})
+server.listen(port,hostname,()=>{
+    console.log(`server ruuning at http://${hostname}:${port} `)
+
+})
