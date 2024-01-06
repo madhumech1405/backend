5cbca518b2a8ce650d3061bbe3c093a4c7671927
madhumech1405 <madhumech1405@gmail.com>
2024-01-06 11:13:55 +0530

    tesk

--- server.js
+++ server.js
@@ -1,28 +1,4 @@
-const express = require('express')
-const app = express()
-const fs = require('fs')
-app.use(express.json())
-
-let ts = Date.now();
-
-let date_ob = new Date(ts);
-let date = date_ob.getDate();
-let month = date_ob.getMonth() + 1;
-let year = date_ob.getFullYear();
-
-
-console.log(year + "-" + month + "-" + date);
-const timestamp = year + "-" + month + "-" + date
-fs.writeFile('date-time.txt',`${timestamp}`,(err) => {
-    if(err) throw err
-    console.log('File Created!!!')
-
-})
-app.get('/',function(req,res){
-    res.json(timestamp)
-})
-
-app.listen(3001)const express = require('express')
+const  express = require('express')
 const app = express()
 const fs = require('fs')
 app.use(express.json())
@@ -47,3 +23,4 @@ app.get('/',function(req,res){
 })
 
 app.listen(3001)
+
